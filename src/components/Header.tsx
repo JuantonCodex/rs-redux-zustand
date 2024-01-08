8a3ffc1b8ea71e095c31556742e89ce8352032b8
Juan Antonio <jcahuanam@gmail.com>
2024-01-07 20:27:46 -0500

    feat: add destaque de aula actual

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
+import { useAppSelector } from "../store";
+
 export function Header() {
+  const { moduleTitle, lessonTitle } = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player;
+
+    const currentModule = state.player.course.modules[currentModuleIndex];
+    const currentLesson = currentModule.lessons[currentLessonIndex];
+
+    return {
+      moduleTitle: currentModule.title,
+      lessonTitle: currentLesson.title,
+    };
+  });
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-      <span className="text-sm text-zinc-400">
-        Módulo "Desvendando o Redux"
-      </span>
+      <h1 className="text-2xl font-bold">{lessonTitle}</h1>
+      <span className="text-sm text-zinc-400">{moduleTitle}</span>
     </div>
   );
 }
