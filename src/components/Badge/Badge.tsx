86a61cc195f3a7d863d190197f82828550dab4c0
Juan Antonio <jcahuanam@gmail.com>
2024-01-17 00:59:22 -0500

    fix: badge partial and styles

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,17 @@
+import { TBadgeVariant } from "../../theme/types";
+import { getClassNames } from "./Badge.styles";
+
+interface IBadgeProps {
+  variant?: TBadgeVariant;
+  color?: "primary" | "secondary";
+  children: React.ReactNode;
+}
+
+export function Button({
+  variant = "standard",
+  color = "primary",
+  children,
+}: Readonly<IBadgeProps>) {
+  const classNames = getClassNames(variant, color);
+  return <div className={classNames}>{children}</div>;
+}
