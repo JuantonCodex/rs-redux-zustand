5165c26d20186ef68b637ff0a093869619b0c5c2
Juan Antonio <jcahuanam@gmail.com>
2024-01-21 16:07:58 -0500

    feat: update axios client config and add new google key

--- src/clients/axios/index.ts
+++ src/clients/axios/index.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
-import { endpoint } from "../../api/constants";
+import { videoEndpoint } from "../../api/constants";
+import { getGlobalConfiguration } from "../../configuration/global";
 
 export const axiosClient = () => {
   return axios.create({
-    baseURL: endpoint,
-    timeout: 1000,
+    baseURL: videoEndpoint,
+    timeout: 3000,
+    params: {
+      key: getGlobalConfiguration().google.API_KEY,
+    },
     headers: {
       Accept: "application/json",
     },
--- src/configuration/global.ts
+++ src/configuration/global.ts
@@ -2,14 +2,22 @@ interface IEcommerceEndpoint {
   API_TOKEN: string;
 }
 
+interface IGoogle {
+  API_KEY: string;
+}
+
 interface IGlobalConfiguration {
   ecommerceEndpoint: IEcommerceEndpoint;
+  google: IGoogle;
 }
 
 export function getGlobalConfiguration(): IGlobalConfiguration {
   return {
     ecommerceEndpoint: {
       API_TOKEN: import.meta.env.VITE_ECOMMERCE_API_TOKEN,
+    },
+    google: {
+      API_KEY: import.meta.env.VITE_GOOGLE_API_KEY,
     },
   };
 }
