3629b29784b6ac02bcbfa55b02c3e0f1386df302
Juan Antonio <jcahuanam@gmail.com>
2024-02-11 01:17:24 -0500

    feat: add new reducers to save the playlist in the store

--- src/features/videoPlayer/store/slices/player.spec.ts
+++ src/features/videoPlayer/store/slices/player.spec.ts
@@ -1,5 +1,5 @@
 import { COURSE_DATA_MOCKED } from "./__mocks__/couse.mock";
-import { player as reducer, play, next } from "./player";
+import { player as reducer, play, next } from "./player.slice";
 
 describe("player slice", () => {
   it("should be able to play", () => {
@@ -10,26 +10,26 @@ describe("player slice", () => {
         videoIndex: 1,
       }),
     );
-    expect(state.currentVideoListIndex).toEqual(1);
-    expect(state.currentVideoIndex).toEqual(1);
+    expect(state.currentGroupIndex).toEqual(1);
+    expect(state.currentElementIndex).toEqual(1);
   });
 
   it("should be able to play next video automatically", () => {
     const state = reducer(COURSE_DATA_MOCKED, next());
-    expect(state.currentVideoListIndex).toEqual(0);
-    expect(state.currentVideoIndex).toEqual(1);
+    expect(state.currentGroupIndex).toEqual(0);
+    expect(state.currentElementIndex).toEqual(1);
   });
 
   it("should be able to jump to the next module", () => {
     const state = reducer(
       {
         ...COURSE_DATA_MOCKED,
-        currentVideoListIndex: 0,
-        currentVideoIndex: 1,
+        currentGroupIndex: 0,
+        currentElementIndex: 1,
       },
       next(),
     );
-    expect(state.currentVideoListIndex).toEqual(1);
-    expect(state.currentVideoIndex).toEqual(0);
+    expect(state.currentGroupIndex).toEqual(1);
+    expect(state.currentElementIndex).toEqual(0);
   });
 });
